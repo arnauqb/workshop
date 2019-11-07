131ce4d52e6a74cc4ae6b9ee31748d4fa457e805
Arnau <arnau.quera@gmail.com>
2019-11-07 20:21:58 +0000

    first commit

---
+++ mean.py
@@ -0,0 +1,2 @@
+def mean(number_list):
+    return sum(number_list) / len(number_list)
---
+++ test_mean.py
@@ -0,0 +1,8 @@
+from mean import mean
+
+def test_results():
+    num_list = [1,2,3]
+    expected = 2
+    assert mean(num_list) == expected
+
+
